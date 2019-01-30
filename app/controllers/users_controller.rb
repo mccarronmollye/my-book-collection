6d47ec2e9309935535af93803fac7dcf91cbd4fa
Molly McCarron <mccarronmollye@gmail.com>
2019-01-30 14:39:39 -0500

    Added login feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+#require "./app/models/users"
+
 class UsersController < ApplicationController
 
   # GET: /users
@@ -7,33 +9,78 @@ class UsersController < ApplicationController
   end
 
   # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  get "/signup" do
+    erb :"/users/signup.html"
   end
 
   # POST: /users
   post "/users" do
     @user = User.create(params[:user])
-    redirect "/users"
+
+    if @user.save
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
+  end
+
+  #failure redirect
+  get "/failure" do
+    erb :"/users/failure.html"
+  end
+
+  #GET: /users/login
+  get "/login" do
+    erb :"/users/login.html"
+  end
+
+  #POST: /login
+  post "/login" do
+    user = User.find_by(:email => params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      erb :"/users/account.html"
+      #redirect "/account"
+    else
+      redirect "/failure"
+    end
+  end
+
+  #GET: successful login!
+  #get "/account" do
+  #  binding.pry
+  #  @user = User.find(session[:user_id])
+  #  erb :"/users/account.html"
+  #end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
   # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
+#  get "/users/:id" do
+#    erb :"/show.html"
+#  end
 
   # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
+#  get "/users/:id/edit" do
+#    erb :"/edit.html"
+#  end
 
   # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
+#  patch "/users/:id" do
+#    redirect "/users/:id"
+#  end
 
   # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
+#  delete "/users/:id/delete" do
+#    redirect "/users"
+#  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190129193710) do
+ActiveRecord::Schema.define(version: 20190130173233) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.string   "genre"
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
